5f7ccdcd9e80f43033189ebcb2f3cae7968c5153
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-08 14:53:27 +0100

    added links to list

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,20 +3,20 @@
     <header>
       <div class="container">
         <div class="logo">
-          <img src="" alt="DC Logo">
+          <img src="" alt="DC Logo" />
         </div>
         <nav class="nav-menu">
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#">link1</a></li>
+            <li><a href="#">link2</a></li>
+            <li><a href="#">link3</a></li>
+            <li><a href="#">link4</a></li>
+            <li><a href="#">link5</a></li>
+            <li><a href="#">link6</a></li>
+            <li><a href="#">link7</a></li>
+            <li><a href="#">link8</a></li>
+            <li><a href="#">link9</a></li>
+            <li><a href="#">link10</a></li>
           </ul>
         </nav>
       </div>
@@ -26,10 +26,9 @@
 
 <script>
 export default {
-  name: '',
-}
+  name: "",
+};
 </script>
 
 <style scoped lang="scss">
-
 </style>
