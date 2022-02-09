6bd37581116805b1b9435e62992431c98a56adea
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-09 17:14:46 +0100

    added comics component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,12 +6,7 @@
 				<div class="currentSeries">
 					<p>CURRENT SERIES</p>
 				</div>
-				<ul>
-					<li v-for="(comic, index) in comics" :key="index">
-						<div class="card"><img :src="comic.thumb" :alt="comic.type" /></div>
-						<p class="comic-series">{{ comic.series }}</p>
-					</li>
-				</ul>
+				<Comics />
 				<div class="load-more">
 					<p>LOAD MORE</p>
 				</div>
@@ -34,8 +29,13 @@
 </template>
 
 <script>
+import Comics from "./Comics.vue";
+
 export default {
 	name: "Main",
+	components: {
+		Comics,
+	},
 	data() {
 		return {
 			products: [
@@ -70,92 +70,6 @@ export default {
 					resized: false,
 				},
 			],
-			comics: [
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
-					price: "$19.99",
-					series: "Action Comics",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
-					price: "$3.99",
-					series: "American Vampire 1976",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
-					price: "$16.99",
-					series: "Aquaman",
-					type: "graphic novel",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
-					price: "$2.99",
-					series: "Batgirl",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
-					price: "$3.99",
-					series: "Batman",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
-					price: "$2.99",
-					series: "Batman Beyond",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
-					price: "$3.99",
-					series: "Batman/Superman",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
-					price: "$4.99",
-					series: "Batman/Superman Annual",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
-					price: "$5.99",
-					series: "Batman: The Joker War Zone",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
-					price: "$6.99",
-					series: "Batman: Three Jokers",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
-					price: "$4.99",
-					series: "Batman: White Knight Presents: Harley Quinn",
-					type: "comic book",
-				},
-				{
-					thumb:
-						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
-					price: "$16.99",
-					series: "Catwoman",
-					type: "graphic novel",
-				},
-			],
 		};
 	},
 };
@@ -166,7 +80,15 @@ export default {
 
 main {
 	color: white;
-	background-color: #1C1C1C;
+	background-color: #1c1c1c;
+
+	.container {
+		position: relative;
+		padding: 50px 0;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
 
 	#jumbotron {
 		background-image: url("../assets/img/jumbotron.jpg");
@@ -176,90 +98,54 @@ main {
 		height: 400px;
 	}
 
-	#comics {
-		.container {
-			position: relative;
-			padding: 50px 0;
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: center;
-		}
-
-		.currentSeries {
-			width: fit-content;
-			padding: 10px;
-			font-weight: bold;
-			background-color: dodgerblue;
-			position: absolute;
-			top: -20px;
-			left: 0;
-			cursor: pointer;
-		}
-
-		ul {
-			list-style-type: none;
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: space-around;
-
-			li {
-				margin: 20px 5px;
-        width: 200px;
-			}
-
-			.card {
-				width: 200px;
-				height: 200px;
-				overflow: hidden;
-			}
-
-      p {
-        padding: 20px 0;
-      }
-			.comic-series {
-				text-transform: uppercase;
-			}
-		}
-
-		.load-more {
-			width: 200px;
-			text-align: center;
-			padding: 10px;
-			font-weight: bold;
-			background-color: dodgerblue;
-      cursor: pointer;
-		}
+	.currentSeries {
+		width: fit-content;
+		padding: 10px;
+		font-weight: bold;
+		background-color: dodgerblue;
+		position: absolute;
+		top: -20px;
+		left: 0;
+		cursor: pointer;
 	}
 
-	#secondary-nav {
+	.load-more {
+		width: 200px;
+		text-align: center;
+		padding: 10px;
+		font-weight: bold;
 		background-color: dodgerblue;
-		padding: 30px 0;
+		cursor: pointer;
+	}
+}
 
-		ul {
-			list-style-type: none;
+#secondary-nav {
+	background-color: dodgerblue;
+
+	ul {
+		list-style-type: none;
+		display: flex;
+		justify-content: space-around;
+
+		li {
+			padding: 10px;
 			display: flex;
-			justify-content: space-around;
+			align-items: center;
 
-			li {
-				padding: 10px;
-				display: flex;
-				align-items: center;
+			img {
+				width: 50px;
+				height: 56px;
+				margin-right: 10px;
+				color: $secondary_color;
+			}
 
-				img {
-					width: 60px;
-					height: 66px;
-					margin-right: 10px;
-          color: $secondary_color;
-				}
+			img.resized {
+				width: 40px;
+			}
 
-				img.resized {
-					width: 50px;
-				}
-
-				a {
-					color: $secondary_color;
-					font-weight: bolder;
-				}
+			a {
+				color: $secondary_color;
+				font-weight: bolder;
 			}
 		}
 	}
